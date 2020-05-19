1a7f20cf635db48a9191b71fb9e434b21a59a013
Lars <lhartvik@gmail.com>
2020-05-19 09:26:18 +0200

    Initial commit

---
+++ src/main/kotlin/no/teleplan/profilregistrering/ProfilregistreringApplication.kt
@@ -0,0 +1,11 @@
+package no.teleplan.profilregistrering
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProfilregistreringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProfilregistreringApplication>(*args)
+}
---
+++ src/test/kotlin/no/teleplan/profilregistrering/ProfilregistreringApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.teleplan.profilregistrering
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProfilregistreringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
