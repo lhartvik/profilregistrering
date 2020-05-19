32d241068b0165eb75fa8318c018b4f700017a86
Lars <lhartvik@gmail.com>
2020-05-19 11:24:31 +0200

    Lager test for ProfilRepository

--- src/test/kotlin/no/teleplan/profilregistrering/ProfilregistreringApplicationTests.kt
+++ src/test/kotlin/no/teleplan/profilregistrering/ProfilregistreringApplicationTests.kt
@@ -1,13 +1,29 @@
 package no.teleplan.profilregistrering
 
+import no.teleplan.profilregistrering.modell.Profil
+import no.teleplan.profilregistrering.repo.ProfilRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ProfilregistreringApplicationTests {
+class ProfilregistreringApplicationTests(@Autowired val profilRepository: ProfilRepository) {
 
     @Test
     fun contextLoads() {
     }
 
+    @Test
+    internal fun kanLagreProfilTilDatabase() {
+        val profil = Profil(id = null, navn = "Navnet")
+
+        assertThat(profil.id).isNull()
+        profilRepository.save(profil)
+        assertThat(profil.id).isNotNull()
+
+        val lastInRepo = profilRepository.findAll().last()
+        assertThat(lastInRepo.id).isEqualTo(profil.id)
+        assertThat(lastInRepo.navn).isEqualTo("Navnet")
+    }
 }
