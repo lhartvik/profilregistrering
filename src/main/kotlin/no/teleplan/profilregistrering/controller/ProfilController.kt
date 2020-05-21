42093a97a4d22a358e319cb8785ccf4984a2c20b
Lars <lhartvik@gmail.com>
2020-05-21 15:06:47 +0200

    Tester og Integrasjonstest for usikret restcontroller

--- src/main/kotlin/no/teleplan/profilregistrering/controller/ProfilController.kt
+++ src/main/kotlin/no/teleplan/profilregistrering/controller/ProfilController.kt
@@ -1,23 +1,31 @@
 package no.teleplan.profilregistrering.controller
 
 import no.teleplan.profilregistrering.modell.User
-import no.teleplan.profilregistrering.repo.UserRepository
+import no.teleplan.profilregistrering.service.ProfilService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
-class ProfilController(@Autowired private val profilRepository: UserRepository) {
+class ProfilController(@Autowired private val profilService: ProfilService) {
 
     @GetMapping("/profiler")
-    fun getAlleProfiler(): List<User> = profilRepository.findAll().toList()
+    fun getAlleProfiler(): List<User> = profilService.brukere()
 
-    @GetMapping("/profil/{profilId}")
-    fun getProfil(@PathVariable profilId: Long): ResponseEntity<User> =
-            profilRepository.findById(profilId).map { profil ->
-                ResponseEntity.ok(profil)
-            }.orElse(ResponseEntity.notFound().build())
+    @GetMapping("/profiler/{brukerId}")
+    fun getProfil(@PathVariable brukerId: Long): ResponseEntity<User> =
+            profilService.bruker(brukerId)
+                    .map { profil -> ResponseEntity.ok(profil) }
+                    .orElse(ResponseEntity.notFound().build())
+
+    @GetMapping("/profil/{brukerId}")
+    fun getProfilByUserName(@PathVariable brukerId: String): ResponseEntity<User> {
+        val bruker = profilService.bruker(brukerId)
+        return if (bruker != null) ResponseEntity.ok(bruker)
+                else ResponseEntity.notFound().build()
+    }
 
 }
